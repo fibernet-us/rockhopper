a11361e3ab86ec4498b252c9224f3da29441e0e6
wenbian <wenbian@gmail.com>
2013-09-18 17:27:41 -0500

    Added 5 tables (team, project and related) into rockhopper.sql

--- inc/rockhopper.sql
+++ inc/rockhopper.sql
@@ -7,17 +7,17 @@
  * @version  1.11
  * @history
  *   09/03/2013: created out of Rockhopper schema in Perl
- *               added table RH_USER, RH_LOG, RH_TASK
- *   09/12/2013: updated table RH_USER, removed last_activity_id
+ *               added table RH_USER, RH_TASK, RH_SUBTASK, RH_USERLOG
  *
+ *   09/18/2013: added tables RH_TEAM, RH_TEAM_MEMBER, RH_PRODUCT, 
+ *                            RH_PRODUCT_TEAM, RH_MESSAGE
  *
  *
  *
  * @notes
  * in some cases foreign keys are not 'cascade on delete', so as to preserve 
  * certain useful information, and in those cases the foreign key is usually 
- * set to 0 to indicate its invalidness, so check for 0 when appropriate.
- * (all valid ids start at 1)
+ * set to null, so check for null when appropriate.
  *
  */
 
@@ -27,12 +27,12 @@
  * table RH_USER
  *
  * stores rockhopper related user info.
- * team list is stored in rhteam_member.
+ * team list is stored in RH_TEAM_member.
  *
  * == type ==
  *   0 UNKNOWN
  *   1 RH_ADMIN
- *   2 PROJECT_OWNER
+ *   2 PRODUCT_OWNER
  *   3 DEV
  *   4 SCRUM_MASTER
  *   5 CHICKEN
@@ -59,39 +59,63 @@ CREATE TABLE RH_USER (
   timezone          tinyint       NOT NULL  DEFAULT 0, 
   location          varchar(255)            DEFAULT NULL,
   iconurl           varchar(255)            DEFAULT NULL,
+
   enabled           tinyint(1)    NOT NULL  DEFAULT 1,
 
   PRIMARY KEY (id),
   UNIQUE KEY uk_RH_USER_username (username),
   UNIQUE KEY uk_RH_USER_email (email)
-  
-  /* CONSTRAINT fk_RH_USER_last_activity_id_RH_USERLOG_id FOREIGN KEY (last_activity_id) REFERENCES RH_USERLOG (id) */
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 /******************************************************************************
  *
- * table RH_USERLOG
+ * table RH_TEAM
  *
- * logs user activities
+ * stores basic team info. 
+ * member list is stored in RH_TEAM_MEMBER
  *
  */
-DROP TABLE IF EXISTS RH_USERLOG;
-CREATE TABLE RH_USERLOG (
+DROP TABLE IF EXISTS RH_TEAM;
+CREATE TABLE RH_TEAM (
 
-  id int NOT NULL AUTO_INCREMENT,
-  time datetime NOT NULL,
-  what text NOT NULL,
-  url tinytext DEFAULT NULL,
+  id           smallint      NOT NULL  AUTO_INCREMENT,
+  name         varchar(255)  NOT NULL,
+  description  text(1024)    NOT NULL,
+  isactive     tinyint       NOT NULL  DEFAULT 0,
+  icon_url     varchar(255)            DEFAULT NULL,
 
-  user_id smallint NOT NULL,   
-  task_id int NOT NULL,       
+  PRIMARY KEY (id),
+  UNIQUE KEY uk_RH_TEAM_name (name)
 
-  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-  /* CONSTRAINT fk_RH_USERLOG_user_id_RH_USER_id FOREIGN KEY (user_id) REFERENCES RH_USER (id), */
-  /* CONSTRAINT fk_RH_USERLOG_task_id_RH_TASK_id FOREIGN KEY (task_id) REFERENCES RH_TASK (id)  */
+
+/******************************************************************************
+ *
+ * table RH_TEAM_MEMBER
+ *
+ * stores team member info: team id - user id
+ *
+ */
+DROP TABLE IF EXISTS RH_TEAM_MEMBER;
+CREATE TABLE RH_TEAM_MEMBER (
+
+  team_id    smallint  NOT NULL,
+  user_id    smallint  NOT NULL,
+
+  PRIMARY KEY (team_id, user_id),
+
+  CONSTRAINT fk_RH_TEAM_MEMBER_team_id_RH_TEAM_id 
+             FOREIGN KEY (team_id) REFERENCES RH_TEAM (id) 
+             ON UPDATE CASCADE
+             ON DELETE CASCADE,
+
+  CONSTRAINT fk_RH_TEAM_MEMBER_user_id_RH_USER_id 
+             FOREIGN KEY (user_id) REFERENCES RH_USER (id)
+             ON UPDATE CASCADE
+             ON DELETE CASCADE
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -100,9 +124,8 @@ CREATE TABLE RH_USERLOG (
  *
  * table RH_TASK
  *
- * stores project story, task, and subtask.
- * task dependency is stored in rhsubtask.
- * backlog task info is stored in rhbacklog.
+ * stores PRODUCT story, task, and subtask.
+ * task dependency is stored in RH_SUBTASK.
  *
  * == type ==
  *  0 FEATURE
@@ -127,6 +150,7 @@ CREATE TABLE RH_USERLOG (
  */
 DROP TABLE IF EXISTS RH_TASK;
 CREATE TABLE RH_TASK (
+
   id               int       NOT NULL  AUTO_INCREMENT,
   name             tinytext  NOT NULL,
   description      text      NOT NULL,
@@ -144,43 +168,187 @@ CREATE TABLE RH_TASK (
   adjusted_time    smallint  NOT NULL  DEFAULT 0,  
   remaining_time   smallint  NOT NULL  DEFAULT 0, 
 
-  parent_id        int                 DEFAULT NULL,
-  creator_id       smallint  NOT NULL,
+  creator_id       smallint,
 
   PRIMARY KEY (id),
-  UNIQUE KEY uk_RH_TASK_priority (priority)
+  UNIQUE KEY uk_RH_TASK_priority (priority),
 
-  /* CONSTRAINT fk_RH_TASK_parent_id_RH_TASK_id FOREIGN KEY (parent_id) REFERENCES RH_TASK (id) ON DELETE CASCADE ON UPDATE CASCADE,   */
-  /* CONSTRAINT fk_RH_TASK_creator_id_RH_USER_id FOREIGN KEY (creator_id) REFERENCES RH_USER (id) ON DELETE CASCADE ON UPDATE CASCADE, */
+  CONSTRAINT fk_RH_TASK_creator_id_RH_USER_id
+             FOREIGN KEY (creator_id) REFERENCES RH_USER (id)
+             ON UPDATE CASCADE
+             ON DELETE SET NULL   /* delete a task's owner should not delete the task itself */
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 /******************************************************************************
  *
- * Create relationships between RH_USER, RH_USERLOG, RH_TASK
+ * table RH_SUBTASK
+ * 
+ * stores task dependency: task id - subtask id
  *
  */
+DROP TABLE IF EXISTS RH_SUBTASK;
+CREATE TABLE RH_SUBTASK (
 
-ALTER TABLE RH_USERLOG 
-    ADD CONSTRAINT fk_RH_USERLOG_user_id_RH_USER_id
-    FOREIGN KEY(user_id)
-    REFERENCES RH_USER (id);
+  task_id      int  NOT NULL,
+  subtask_id   int  NOT NULL,
 
-ALTER TABLE RH_USERLOG 
-    ADD CONSTRAINT fk_RH_USERLOG_task_id_RH_TASK_id
-    FOREIGN KEY(task_id)
-    REFERENCES RH_TASK (id);
+  PRIMARY KEY (task_id, subtask_id),
 
-ALTER TABLE RH_TASK 
-    ADD CONSTRAINT fk_RH_TASK_parent_id_RH_TASK_id
-    FOREIGN KEY (parent_id)
-    REFERENCES RH_TASK (id)
-    ON DELETE CASCADE ON UPDATE CASCADE;
+  CONSTRAINT fk_RH_SUBTASK_task_id_RH_TASK_id 
+             FOREIGN KEY (task_id) REFERENCES RH_TASK (id)
+             ON UPDATE CASCADE
+             ON DELETE CASCADE,
 
-ALTER TABLE RH_TASK 
-    ADD CONSTRAINT fk_RH_TASK_creator_id_RH_USER_id
-    FOREIGN KEY (creator_id)
-    REFERENCES RH_USER (id)
-    ON DELETE CASCADE ON UPDATE CASCADE;
+  CONSTRAINT fk_RH_SUBTASK_subtask_id_RH_TASK_id 
+             FOREIGN KEY (subtask_id) REFERENCES RH_TASK (id) 
+             ON UPDATE CASCADE
+             ON DELETE CASCADE
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+/******************************************************************************
+ *
+ * table RH_USERLOG
+ *
+ * logs user activities
+ *
+ */
+DROP TABLE IF EXISTS RH_USERLOG;
+CREATE TABLE RH_USERLOG (
+
+  id           int       NOT NULL AUTO_INCREMENT,
+  creation_ts  datetime  NOT NULL,
+  what         text      NOT NULL,
+  url          tinytext             DEFAULT NULL,
+
+  user_id      smallint  NOT NULL,   
+  task_id      int                  DEFAULT NULL,
+
+  PRIMARY KEY (id),
+
+  CONSTRAINT fk_RH_USERLOG_user_id_RH_USER_id
+             FOREIGN KEY(user_id) REFERENCES RH_USER (id)
+             ON UPDATE CASCADE
+             ON DELETE CASCADE,
+
+  CONSTRAINT fk_RH_USERLOG_task_id_RH_TASK_id
+             FOREIGN KEY(task_id) REFERENCES RH_TASK (id)
+             ON UPDATE CASCADE
+             ON DELETE CASCADE
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+/******************************************************************************
+ *
+ * table RH_PRODUCT
+ *
+ * stores basic PRODUCT info. 
+ * backlog info is in RH_BACKLOG.
+ * team list is stored in RH_PRODUCT_RH_TEAM
+ *
+ * == status == 
+ *   0: NOT_READY
+ *   1: READY
+ *   2: IN_PROGRESS
+ *   3. PAUSED
+ *   4. TESTING
+ *   5. COMPLETED
+ *   6. ABORTED
+ */
+DROP TABLE IF EXISTS RH_PRODUCT;
+CREATE TABLE RH_PRODUCT (
+
+  id             smallint     NOT NULL AUTO_INCREMENT,
+  name           varchar(255) NOT NULL,
+  description    text(65535)  NOT NULL,
+  status         tinyint      NOT NULL DEFAULT 0,
+  date_start     date                  DEFAULT NULL,
+  date_deadline  date                  DEFAULT NULL,
+  date_end       date                  DEFAULT NULL,
+  date_lud       date                  DEFAULT NULL, /* last update date */
+  icon_url       varchar(255)          DEFAULT NULL,
+
+  owner_id       smallint,
+
+  PRIMARY KEY (id),
+  UNIQUE KEY uk_RH_PRODUCT_name (name),
+
+  CONSTRAINT fk_RH_PRODUCT_owner_id_RH_USER_id 
+             FOREIGN KEY (owner_id) REFERENCES RH_USER (id)
+             ON UPDATE CASCADE
+             ON DELETE SET NULL
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+/******************************************************************************
+ *
+ * table RH_PRODUCT_TEAM
+ *
+ * stores product team info: product id - team id
+ *
+ */
+DROP TABLE IF EXISTS RH_PRODUCT_TEAM;
+CREATE TABLE RH_PRODUCT_TEAM (
+
+  product_id  smallint NOT NULL,
+  team_id  smallint  NOT NULL,
+
+  PRIMARY KEY (product_id, team_id),
+
+  CONSTRAINT fk_RH_PRODUCT_TEAM_product_id_RH_PRODUCT_id 
+             FOREIGN KEY (product_id) REFERENCES RH_PRODUCT (id)
+             ON UPDATE CASCADE
+             ON DELETE CASCADE,
+
+  CONSTRAINT fk_RH_PRODUCT_TEAM_team_id_RH_TEAM_id 
+             FOREIGN KEY (team_id) REFERENCES RH_TEAM (id) 
+             ON UPDATE CASCADE
+             ON DELETE CASCADE
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+/******************************************************************************
+ *
+ * table RH_MESSAGE
+ * 
+ * stores user message and request.
+ *
+ * == status ==
+ *   0 NOT_READ
+ *   1 IS_READ
+ *   2 IS_REPLIED
+ *   3 IS_CLOSED   # for request only
+ *
+ */
+DROP TABLE IF EXISTS RH_MESSAGE;
+CREATE TABLE RH_MESSAGE (
+
+  id           int           NOT NULL  AUTO_INCREMENT,
+  creation_ts  datetime      NOT NULL,
+  status       tinyint       NOT NULL  DEFAULT 0,
+  subject      tinytext      NOT NULL,
+  content      text          NOT NULL,
+  
+  from_id      smallint      NOT NULL,
+  to_id        smallint      NOT NULL,
+
+  PRIMARY KEY (id),
+
+  CONSTRAINT fk_RH_MESSAGE_from_id_RH_USER_id 
+             FOREIGN KEY (from_id) REFERENCES RH_USER (id)
+             ON UPDATE CASCADE
+             ON DELETE CASCADE,
+
+  CONSTRAINT fk_RH_MESSAGE_to_id_RH_USER_id 
+             FOREIGN KEY (to_id) REFERENCES RH_USER (id)
+             ON UPDATE CASCADE
+             ON DELETE CASCADE
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
