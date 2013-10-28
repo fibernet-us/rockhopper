56b6e4e0b2c9de6635fabbba5597c9c524590a89
annestel <estel.z@gmail.com>
2013-10-28 00:02:37 -0400

    updated checkavailable.php, including user.php
    replaced my code with PDO API

--- rockhopper/checkavailable.php
+++ rockhopper/checkavailable.php
@@ -4,23 +4,21 @@
  //mysql_select_db("rockhopper");
  
  require_once 'connect.php';
+ require_once 'user.php';
  
  $checkuser = $_GET["checkuser"];
  $nametype = $_GET["nametype"];
  
  if ($nametype == "isname") {
-	 $sql = "SELECT * FROM  RH_USER where username='$checkuser'";
-	 $stmt = $dbh->prepare($sql);
-	 $stmt->execute();
-	 if ($stmt->rowCount() == 0) echo 0;
+	 $checkresult = User::isUsernameRegistered($dbh, $checkuser);
+	 if ($checkresult == false) echo 0;
 	 else echo 1;
  }
+
  
  else if ($nametype == "isemail") {
-	 $sql = "SELECT * from RH_USER where email='$checkuser'";
-	 $stmt = $dbh->prepare($sql);
-	 $stmt->execute();
-	 if ($stmt->rowCount() == 0) echo 2;
+	 $checkresult = User::isEmailRegistered($dbh, $checkuser);
+	 if ($checkresult == false) echo 2;
 	 else echo 3;
  }
  
